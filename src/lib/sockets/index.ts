ac581799535de46b641db1d7359a25d36f44063e
kunal-fushiguro <natwarlaluzumaki@gmail.com>
2024-10-22 18:35:40 +0530

    update: Updated Teams section

--- src/lib/sockets/index.ts
+++ src/lib/sockets/index.ts
@@ -1,11 +1,11 @@
 import { io } from "socket.io-client";
-export const socket = io("http://localhost:7145", {
-  transports: ["polling"],
-  // upgrade: false,
-  // reconnectionAttempts: Infinity,
-  reconnectionDelay: 1000,
-  // rejectUnauthorized: false,
-  // forceNew: true,
-  // path: '/socket.io',
-});
-// export const socket = io("http://localhost:7145");
+// export const socket = io("http://localhost:7145", {
+//   transports: ["polling"],
+//   // upgrade: false,
+//   // reconnectionAttempts: Infinity,
+//   reconnectionDelay: 1000,
+//   // rejectUnauthorized: false,
+//   // forceNew: true,
+//   // path: '/socket.io',
+// });
+export const socket = io("http://localhost:7145");
