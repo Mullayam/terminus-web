ce2978b2e8babe01e517fb9caa3b5b903fe18a75
Mullayam <mullayam06@gmail.com>
2024-11-18 06:15:18 +0530

    updated env and api url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
-const URL = import.meta.env.VITE_APP_ENV === "PROD"? "https://terminus-web-api.onrender.com" : 'http://localhost:7145';
+const URL = import.meta.env.VITE_APP_ENV === "PROD"? import.meta.env.VITE_API_URL : 'http://localhost:7145';
 const config = {
     API_URL: URL,
     APP_ENV: import.meta.env.VITE_APP_ENV,
